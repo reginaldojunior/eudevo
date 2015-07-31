0a32df79c77e4de14be27d8789bf3aaa37a267ed
Reginaldo Junior <76regi@gmail.com>
2015-07-30 22:04:27 -0300

    User create table ok

--- src/Entities/User.php
+++ src/Entities/User.php
@@ -1,4 +1,26 @@
 <?php  
+/*
+
+CREATE TABLE IF NOT EXISTS `users` (
+`id` int(11) NOT NULL,
+  `name` varchar(60) NOT NULL,
+  `last_name` varchar(50) NOT NULL,
+  `login` varchar(100) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `facebook_id` int(11) NOT NULL,
+  `date_birth` date NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `img` varchar(150) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+ALTER TABLE `users`
+ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `users`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=1;
+
+*/
+
 namespace Entities;
 
 /**
@@ -37,14 +59,20 @@ class User {
   private $facebook_id;
 
   /**
-  * @Column(name="date_birth", type="date_birth")
+  * @Column(name="date_birth", type="date")
   **/
   private $date_birth;
 
   /**
-  * @Column(name="active", type="tyint")
+  * @Column(name="active", type="integer")
   **/
   private $active;
+
+  /**
+  * @Column(name="img", type="string")
+  **/
+  private $img;
+
 
   public function getId() {
     return $this->id;
